a784caa150aabf9f3592cd2856802971fdaf7b56
vladislavf50 <vladislavf50@yandex.ru>
2016-02-12 11:18:11 +0300

    Конструкторы

--- MyFirstSnake/MyFirstSnake/Program.cs
+++ MyFirstSnake/MyFirstSnake/Program.cs
@@ -10,28 +10,11 @@ namespace MyFirstSnake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-            p1.Draw();
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw(); 
 
-            /*int x1 = 1;
-              int y1 = 3;
-                  char sym1 = '*';
-              Draw(x1, x1, sym1); */
-
-            Point p2 = new Point();
-            p2.x =4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
-
-            /*
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2); */
 
             Console.ReadLine();
         }
