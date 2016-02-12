3efe51eae82f450fa96ecfba04ee3459411e9961
vladislavf50 <vladislavf50@yandex.ru>
2016-02-12 11:45:11 +0300

    Лист...

--- MyFirstSnake/MyFirstSnake/Program.cs
+++ MyFirstSnake/MyFirstSnake/Program.cs
@@ -16,13 +16,26 @@ namespace MyFirstSnake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            List<int> numlist = new List<int>();
+            numlist.Add(0);
+            numlist.Add(1);
+            numlist.Add(2);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+
+            foreach(int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+            numlist.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
+
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-
     }
 }
